ce56770fd9aaa62064a28523da9a833175c38597
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-12-20 11:12:15 -0300

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,1 @@
+//Insira o código aqui
---
+++ navigation/TabNavigator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "../screens/Feed";
+import CreatePost from "../screens/CreatePost";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return (
+        //Insira o código aqui
+    );
+}
+
+export default BottomTabNavigator
